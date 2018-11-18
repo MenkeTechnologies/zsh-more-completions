8102a44a797841c64c756e31134f542832c40b91
MenkeTechnologies <linux.dev25@gmail.com>
2018-11-17 21:34:11 -0500

    zsh completions from debian

---
+++ src/_asip-status.pl
@@ -0,0 +1,13 @@
+#compdef asip-status.pl
+
+local arguments
+
+arguments=(
+  '-d[: Enable debug output.]'
+  '-i[: Show icon if it exists.]'
+  '-x[: Enable hex dump output.]'
+  {-v,-version,--version}'[: Show version.]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
---
+++ src/_createTextFile.sh
@@ -0,0 +1,19 @@
+#compdef createTextFile.sh
+
+local arguments
+
+arguments=(
+  {-c,--console}'[use the given VT number;]'
+  {-e,--exec}'[execute the command, without forking;]'
+  {-f,--force}'[force opening a VT without checking;]'
+  {-l,--login}'[make the command a login shell;]'
+  {-u,--user}'[figure out the owner of the current VT;]'
+  {-s,--switch}'[switch to the new VT;]'
+  {-w,--wait}'[wait for command to complete;]'
+  {-v,--verbose}'[print a message for each action;]'
+  {-V,--version}'[print program version and exit;]'
+  {-h,--help}'[output a brief help message.]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
