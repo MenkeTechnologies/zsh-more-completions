52b91daaa6823e469aa75fa01b1207ccb8617b9c
MenkeTechnologies <linux.dev25@gmail.com>
2018-11-26 02:46:51 -0500

    comps from debian

---
+++ src/_pipes.sh
@@ -0,0 +1,22 @@
+#compdef pipes.sh
+
+local arguments
+
+arguments=(
+  '-p[1-\]               number of pipes (D=1)]'
+  '-t[0-9\]              pipe type (D=0)]'
+  '-t[16 chars\]        custom pipe type]'
+  '-c[0-256\]            pipe color INDEX (TERM=xterm-256color), can be]'
+  '-f[20-100\]           framerate (D=75)]'
+  '-s[5-15\]             going straight probability, 1 in (D=13)]'
+  '-r[0-\]               reset after (D=2000) characters, 0 if no reset]'
+  '-R[randomize starting position and direction]'
+  '-B[no bold effect]'
+  '-C[no color]'
+  '-K[keep pipe color and type when crossing edges]'
+  '-h[print this help message]'
+  '-v[print version number]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
