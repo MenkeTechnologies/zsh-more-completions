0c4cad92c6c3b17711610354dcc6731bb06fc0f0
MenkeTechnologies <linux.dev25@gmail.com>
2018-12-01 22:01:22 -0500

    more kali completions

---
+++ src/_BlindElephant.py
@@ -0,0 +1,16 @@
+#compdef BlindElephant.py
+
+local arguments
+
+arguments=(
+  {-h,--help}'[show this help message and exit]'
+  {-p,--pluginName}'[fingerprint version of plugin (should apply to web app]'
+  {-s,--skip}'[skip fingerprinting webpp, just fingerprint plugin]'
+  {-n,--numProbes}'[number of files to fetch (more may increase accuracy).]'
+  {-w,--winnow}'[if more than one version are returned, use winnowing]'
+  {-l,--list}'[list supported webapps and plugins]'
+  {-u,--updateDB}'[pull latest DB files from]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
