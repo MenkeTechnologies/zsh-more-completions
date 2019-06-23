621cfdf142429fb959b0dece7cd11354035ea1b7
MenkeTechnologies <linux.dev25@gmail.com>
2019-06-22 23:51:29 -0400

    comps from parrot

---
+++ src/_ntfs-read.py
@@ -0,0 +1,12 @@
+#compdef ntfs-read.py
+
+local arguments
+
+arguments=(
+  {-h,--help}'[show this help message and exit]'
+  '-extract[extracts pathname (e.g. \windows\system32\config\sam)]'
+  '-debug[turn DEBUG output ON]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
