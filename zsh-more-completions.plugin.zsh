b780ddabb47dfa179e9997795df9f1bf897dd47d
MenkeTechnologies <linux.dev25@gmail.com>
2020-05-08 17:55:35 -0400

    add fpath no dups

--- zsh-more-completions.plugin.zsh
+++ zsh-more-completions.plugin.zsh
@@ -1,1 +1,15 @@
-fpath=("${0:h}/override_src" $fpath "${0:h}/src" "${0:h}/man_src")
+dirsFirst=("${0:h}/override_src")
+dirsLast=("${0:h}/src" "${0:h}/man_src")
+for dir in $dirsFirst[@]; do
+    if [[ -z ${fpath[(r)$dir]} ]];then
+        fpath=($dir $fpath)
+    fi
+done
+for dir in $dirsLast[@]; do
+    if [[ -z ${fpath[(r)$dir]} ]];then
+        fpath=($fpath $dir)
+    fi
+done
+
+unset dirsLast
+unset dirsFirst
