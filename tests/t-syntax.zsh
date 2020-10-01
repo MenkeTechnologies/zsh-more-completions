14f92b3db0fb8ebcde1235b6131aa0b5f2fe968a
MenkeTechnologies <linux.dev25@gmail.com>
2020-10-01 15:50:30 -0400

    add syntax tests

---
+++ tests/t-syntax.zsh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zunit
+#{{{                    MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Tue Feb 25 19:37:50 EST 2020
+##### Purpose: zsh script to
+##### Notes:
+#}}}***********************************************************
+
+@setup {
+    0="${${0:#$ZSH_ARGZERO}:-${(%):-%N}}"
+    0="${${(M)0:#/*}:-$PWD/$0}"
+    pluginDir="${0:h:A}"
+}
+
+@test 'man src' {
+	for _file in "$pluginDir/man_src/"*; do
+        run zsh -n "$_file"
+        assert $state equals 0
+    done
+}
+
+@test 'src' {
+	for _file in "$pluginDir/src/"*; do
+        run zsh -n "$_file"
+        assert $state equals 0
+    done
+}
+
+@test 'override_src' {
+	for _file in "$pluginDir/override_src/"*; do
+        run zsh -n "$_file"
+        assert $state equals 0
+    done
+}
+
+@test 'plugin *.zsh' {
+	for file in "$pluginDir/"*.zsh; do
+        run zsh -n "$file"
+        assert $state equals 0
+    done
+}
