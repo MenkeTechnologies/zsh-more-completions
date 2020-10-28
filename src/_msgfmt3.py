8c6e19ea1bc0e219e7cb6465aad3f81001ac6343
MenkeTechnologies <linux.dev25@gmail.com>
2020-10-27 22:31:57 -0400

    ami comps

---
+++ src/_msgfmt3.py
@@ -0,0 +1,15 @@
+#compdef msgfmt3.py
+
+local arguments
+
+arguments=(
+  '-o[]'
+  '--output-file[specify the output file to write to.  If omitted, output will go to a]'
+  '-h[]'
+  '--help[print this message and exit.]'
+  '-V[]'
+  '--version[display version information and exit.]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
