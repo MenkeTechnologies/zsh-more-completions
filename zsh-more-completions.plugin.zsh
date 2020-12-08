cb7ae6364d0cd8b74db271c79289f75022271a56
MenkeTechnologies <linux.dev25@gmail.com>
2020-12-08 15:56:49 -0500

    prevent namespace collisions

--- zsh-more-completions.plugin.zsh
+++ zsh-more-completions.plugin.zsh
@@ -1,18 +1,18 @@
 # OMZ does not add nested comp dirs to fpath so do it here
 # for this to take effect compinit must be run 2x, once in OMZ and once after sourcing this file
-dirsFirst=("${0:h}/override_src")
-dirsLast=("${0:h}/src" "${0:h}/man_src")
-for dir in $dirsFirst[@]; do
-    if [[ -z ${fpath[(r)$dir]} ]];then
-        fpath=($dir $fpath)
+__zsh_more_comp_dirs_first=("${0:h}/override_src")
+__zsh_more_comp_dirs_last=("${0:h}/src" "${0:h}/man_src")
+for __zsh_more_comp_dir in $__zsh_more_comp_dirs_first[@]; do
+    if [[ -z ${fpath[(r)$__zsh_more_comp_dir]} ]];then
+        fpath=($__zsh_more_comp_dir $fpath)
     fi
 done
-for dir in $dirsLast[@]; do
-    if [[ -z ${fpath[(r)$dir]} ]];then
-        fpath=($fpath $dir)
+for __zsh_more_comp_dir in $__zsh_more_comp_dirs_last[@]; do
+    if [[ -z ${fpath[(r)$__zsh_more_comp_dir]} ]];then
+        fpath=($fpath $__zsh_more_comp_dir)
     fi
 done
 
-unset dirsLast
-unset dirsFirst
-unset dir
+unset __zsh_more_comp_dirs_last
+unset __zsh_more_comp_dirs_first
+unset __zsh_more_comp_dir
