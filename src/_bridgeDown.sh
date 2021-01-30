4aab870129df12f96496130543a896905893ea2d
MenkeTechnologies <linux.dev25@gmail.com>
2021-01-30 15:28:12 -0500

    more --help comps

---
+++ src/_bridgeDown.sh
@@ -0,0 +1,36 @@
+#compdef bridgeDown.sh
+
+local arguments
+
+arguments=(
+  {-h,--help}'[show this help]'
+  {-M,--collect-mobile-documents}'[(default: all containers)]'
+  {-s,--sysdiagnose}'[do not collect whats already part of sysdiagnose]'
+  {-t,--uitest}'[collect logs for UI tests]'
+  {-n,--name}'[change the device name]'
+  {-f,--full}'[do a full diagnose, including server checks]'
+  {-d,--doc}'[collect additional information about the document at that path.]'
+  {-e,--no-reveal}'[do not reveal diagnose in the Finder when done]'
+  {-a,--all}'[show all system logs]'
+  {-p,--predicate}'[additional predicate (see `log help predicates`)]'
+  {-x,--process}'[filter events from the specified process]'
+  {-d,--path}'[use <logs-dir> instead of default]'
+  {-S,--start}'[HH:MM:SS" Start log dump from a specified date]'
+  {-E,--end}'[HH:MM:SS"   Stop log dump after a specified date]'
+  '-b[            Only show CloudDocs logs]'
+  '-f[            Only show FileProvider related logs]'
+  '-F[only show FruitBasket related logs]'
+  '-g[            Only show Genstore related logs]'
+  '-i[only show SQL and CloudDocs logs]'
+  {-z,--local-timezone}'[display timestamps within local timezone]'
+  {-o,--output}'[redirect output to <file-path>]'
+  {-d,--database-path}'[use the database at <db-path>]'
+  {-i,--itemless}'[dont dump items from the db]'
+  {-u,--upgrade}'[upgrade the db if necessary before dumping]'
+  '-g[dump global activity of the iCloud Drive]'
+  '-i[dump changes incrementally]'
+  {-S,--scope}'[restrict the NSMetadataQuery scope to docs, data, external or a combination]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
---
+++ src/_xgettext5.28.pl
@@ -0,0 +1,26 @@
+#compdef xgettext5.28.pl
+
+local arguments
+
+arguments=(
+  {-f,--files-from}'[*]'
+  {-D,--directory}'[*]'
+  {-u,--use-gettext-style}'[specifies that the source programs uses the Gettext style (e.g. %1)]'
+  {-d,--default-domain}'[*]'
+  {-o,--output}'[*]'
+  {-p,--output-dir}'[*]'
+  {-g,--gnu-gettext}'[enables GNU gettext interoperability by printing "#]'
+  {-W,--wrap}'[if wrap is enabled, then, for entries with multiple file locations]'
+  '-P[|--plugin pluginname]'
+  '-P[|--plugin pluginname=*]'
+  '-P[|--plugin pluginname=ext,ext2]'
+  '-P[|--plugin My::Module::Name=*]'
+  '-w[|--warnings]'
+  '-now[|--nowarnings]'
+  '-v[|--verbose]'
+  {-v,-v}'[|--verbose --verbose :]'
+  {-v,-v}'[|--verbose --verbose :]'
+  '*:filename:_files'
+)
+
+_arguments -s $arguments
