bfae1553e86633ff8081fc83f8dd93c3d3060eb5
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-26 23:49:47 -0500

    use anon function

--- zsh-more-completions.plugin.zsh
+++ zsh-more-completions.plugin.zsh
@@ -1,18 +1,18 @@
 # OMZ does not add nested comp dirs to fpath so do it here
 # for this to take effect compinit must be run 2x, once in OMZ and once after sourcing this file
-__zsh_more_comp_dirs_first=("${0:h}/override_src")
-__zsh_more_comp_dirs_last=("${0:h}/src" "${0:h}/man_src")
-for __zsh_more_comp_dir in $__zsh_more_comp_dirs_first[@]; do
-    if [[ -z ${fpath[(r)$__zsh_more_comp_dir]} ]];then
-        fpath=($__zsh_more_comp_dir $fpath)
-    fi
-done
-for __zsh_more_comp_dir in $__zsh_more_comp_dirs_last[@]; do
-    if [[ -z ${fpath[(r)$__zsh_more_comp_dir]} ]];then
-        fpath=($fpath $__zsh_more_comp_dir)
-    fi
-done
+(){
+    local __zsh_more_comp_dirs_first=("$1/override_src")
+    local __zsh_more_comp_dirs_last=("$1/src" "$1/man_src")
+    local __zsh_more_comp_dir
 
-unset __zsh_more_comp_dirs_last
-unset __zsh_more_comp_dirs_first
-unset __zsh_more_comp_dir
+    for __zsh_more_comp_dir in $__zsh_more_comp_dirs_first[@]; do
+        if [[ -z ${fpath[(r)$__zsh_more_comp_dir]} ]];then
+            fpath=($__zsh_more_comp_dir $fpath)
+        fi
+    done
+    for __zsh_more_comp_dir in $__zsh_more_comp_dirs_last[@]; do
+        if [[ -z ${fpath[(r)$__zsh_more_comp_dir]} ]];then
+            fpath=($fpath $__zsh_more_comp_dir)
+        fi
+    done
+} ${0:h}
