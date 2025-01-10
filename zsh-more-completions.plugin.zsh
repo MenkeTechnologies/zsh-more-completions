6d2f9952f3c6e735e93fcad6e50134ca247ffe61
MenkeTechnologies <linux.dev25@gmail.com>
2025-01-10 00:08:19 -0500

    move architecture prefix comps

--- zsh-more-completions.plugin.zsh
+++ zsh-more-completions.plugin.zsh
@@ -2,7 +2,7 @@
 # for this to take effect compinit must be run 2x, once in OMZ and once after sourcing this file
 (){
     local __zsh_more_comp_dirs_first=("$1/override_src")
-    local __zsh_more_comp_dirs_last=("$1/src" "$1/man_src")
+    local __zsh_more_comp_dirs_last=("$1/src" "$1/man_src" "$1/architecture_src")
     local __zsh_more_comp_dir
 
     for __zsh_more_comp_dir in $__zsh_more_comp_dirs_first[@]; do
