0d59516dd93199303106794b54936b784e003dd3
MenkeTechnologies <linux.dev25@gmail.com>
2026-03-28 15:25:00 -0400

    man_src/_a

--- man_src/_afl-cmin.bash
+++ man_src/_afl-cmin.bash
@@ -1,18 +1,19 @@
 #compdef afl-cmin.bash
 
+# Updated with latest AFL++ afl-cmin.bash options
+
 local arguments
 
 arguments=(
-  '-i[- input directory with the starting corpus]'
-  '-o[- output directory for minimized files]'
-  '-f[- location read by the fuzzed program (stdin)]'
-  '-m[- memory limit for child process (200 MB)]'
-  '-t[- run time limit for child process (none)]'
-  '-Q[- use binary-only instrumentation (QEMU mode)]'
-  '-U[- use unicorn-based instrumentation (Unicorn mode)]'
-  '-C[- keep crashing inputs, reject everything else]'
-  '-e[- solve for edge coverage only, ignore hit counts]'
-  '*:filename:_files'
+    '-i[Input directory with the starting corpus]:directory:_directories'
+    '-o[Output directory for minimized files]:directory:_directories'
+    '-f[Location read by the fuzzed program (default: stdin)]:file:_files'
+    '-m[Memory limit for child process in MB (default: 200)]:megabytes'
+    '-t[Run time limit for child process in ms (default: none)]:milliseconds'
+    '-Q[Use binary-only instrumentation (QEMU mode)]'
+    '-U[Use unicorn-based instrumentation (Unicorn mode)]'
+    '-C[Keep crashing inputs, reject everything else]'
+    '-e[Solve for edge coverage only, ignore hit counts]'
+    '*:target command:_files'
 )
-
 _arguments -s $arguments
